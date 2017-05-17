704186f30f6c082953e301e45918463dc7c7f5dd
youyim123 <youyim@hotmail.com>
2017-05-17 11:41:17 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # android_device_nubia_nx531j-twrp
-For building TWRP for theNubia Z11 only 
---
+++ cm.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_NAME := cm_nx531j
+PRODUCT_DEVICE := nx531j
+PRODUCT_MANUFACTURER := nubia
+PRODUCT_BRAND := nubia
+PRODUCT_MODEL := NX531J
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_GMS_CLIENTID_BASE := android-zte
+
+TARGET_VENDOR_PRODUCT_NAME := NX531J
+TARGET_VENDOR_DEVICE_NAME := NX531J
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=NX531J PRODUCT_NAME=NX531J
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=nubia/NX531J/NX531J:7.1.1/NMF26F/nubia03220919:user/release-keys \
+    PRIVATE_BUILD_DESC="NX531J-user 7.1.1 NMF26F eng.nubia.20170322.091929 release-keys"
+
